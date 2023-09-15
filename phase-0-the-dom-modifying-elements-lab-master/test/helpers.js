b500122c52b42b2835362d0d12db8f65039cfd00
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-14 22:16:15 -0400

    phase-1-lecture-videos-changing-dom-elements/
    phase-1-lecture-videos-changing-dom-elements/

--- phase-0-the-dom-modifying-elements-lab-master/test/helpers.js
+++ phase-0-the-dom-modifying-elements-lab-master/test/helpers.js
@@ -1,23 +1,30 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const chai = require('chai');
+global.expect = chai.expect;
+const fs = require('fs');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
 const babel = require('babel-core');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
 
 const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
+  path.resolve(__dirname, '..', 'index.js'),
+  {
     presets: ['env']
   }
 );
 
-const src = babelResult.code
+const src = babelResult.code;
 
-jsdom({
-  html, src
+describe('index.js', () => {
+  jsdom({
+    html,
+    src
+  });
+
+  let newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
+  newHeader.textContent = 'Valencia Walker is the champion';
+
+  // Add your test cases here
 });
-let newHeader = document.createElement('h1');
-newHeader.id = 'victory';
-newHeader.textContent = 'Valencia Walker is the champion';
